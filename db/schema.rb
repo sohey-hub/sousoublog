67b33326eaef3e5c6017002350729070f350e1b8
sohey-hub <stcydgtsc7@gmail.com>
2021-02-24 11:56:47 +0900

    投稿ページのviewの実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_02_23_093607) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
